e53fbb0aa0c282469a9881fa6d8b100920d24824
Dominick Triola <dominick@ignovadesign.com>
2016-04-02 18:52:13 -0700

    created grid

--- gof.js
+++ gof.js
@@ -1,4 +1,40 @@
 var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2D');
+var ctx = canvas.getContext('2d');
 
-console.log("testing");
+var gridRows = 20;
+var gridColumns = 20;
+var cellWidth = canvas.width / gridColumns;
+var cellHeight = canvas.height / gridRows;
+var grid = [];
+
+function setGrid(rows, columns) {
+	for (r=0; r<rows; r++) {
+		grid[r] = []
+		for (c=0; c<columns; c++) {
+			grid[r] = grid[r].concat([cellWidth, cellHeight]);
+		}
+	}
+	return grid;
+}
+
+var grid = setGrid(gridRows, gridColumns);
+
+function drawGrid() {
+	for (r=0; r<grid.length; r++) {
+		for (c=0; c<grid[r].length; c++) {
+			ctx.beginPath();
+			ctx.rect(r*cellWidth, c*cellHeight, cellWidth, cellHeight);
+			ctx.strokeStyle = "#aaa";
+			ctx.stroke();
+		}
+	}
+}
+
+
+
+drawGrid();
+
+console.log(grid[0]);
+
+
+
