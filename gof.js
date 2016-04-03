44915a81e35812ad72e895582e145f78538b353e
Dominick Triola <dominick@ignovadesign.com>
2016-04-02 20:41:56 -0700

    made random cells populate grid on window load, and setup most of runLife function

--- gof.js
+++ gof.js
@@ -1,8 +1,14 @@
+/* Things to fix
+	
+	- add edge cases for runLife()
+
+*/
+
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
-var gridRows = 20;
-var gridColumns = 20;
+var gridRows = 30;
+var gridColumns = 30;
 var cellWidth = canvas.width / gridColumns;
 var cellHeight = canvas.height / gridRows;
 var grid = [];
@@ -11,7 +17,9 @@ function setGrid(rows, columns) {
 	for (r=0; r<rows; r++) {
 		grid[r] = []
 		for (c=0; c<columns; c++) {
-			grid[r] = grid[r].concat([cellWidth, cellHeight]);
+			//randomly set initial life (at 50% chance to be alive)
+			var iniAlive = Math.floor(Math.random()*2);
+			grid[r][c] = { x: r*cellWidth , y: c*cellHeight, isAlive: iniAlive, willBe: null}
 		}
 	}
 	return grid;
@@ -22,19 +30,89 @@ var grid = setGrid(gridRows, gridColumns);
 function drawGrid() {
 	for (r=0; r<grid.length; r++) {
 		for (c=0; c<grid[r].length; c++) {
+			//name cell for easier reference
+			var cell = grid[r][c];
+
+			//draw cell
 			ctx.beginPath();
-			ctx.rect(r*cellWidth, c*cellHeight, cellWidth, cellHeight);
+			ctx.rect(cell.x, cell.y, cellWidth, cellHeight);
 			ctx.strokeStyle = "#aaa";
 			ctx.stroke();
+			if(cell.isAlive) {
+				ctx.fillStyle = "#0056ff";
+				ctx.fill();
+			}
+			ctx.closePath();
 		}
 	}
 }
 
+//acts like a torus??
+function runLife() {
+
+	//determine whether cells will be alive or dead
+	for (r=0; r<grid.length; r++) {
+		for (c=0; c<grid[r].length; c++) {
+			//name cell for easier reference
+			var cell = grid[r][c];
+			//count live neighbors
+			count = 0;
+			
+			/*
+			//for non-edge cases
+			for (i=r-1; i<r+4; i++) {
+				for (j=j-1; j<4; j++)	
+					if (grid[i][j].isAlive && (i!=r && j!=c)) {
+						count+=1;
+					}
+				}
+			}
+			*/
+			
+			if (cell.isAlive) {	
+				if (count > 1 && count < 4) {
+					cell.willBe = 'alive';
+				} else {
+					cell.willBe = 'dead';
+				}
+			} else if (count == 2) {
+				cell.willBe = 'alive';
+			} else {
+				cell.willBe = 'dead';
+			}
+			
+		}
+	}
 
 
+	//assign willBe states to isAlive state
+	for (r=0; r<grid.length; r++) {
+		for (c=0; c<grid[r].length; c++) {
+			//name cell for easier reference
+			var cell = grid[r][c];
+			if (cell.willBe == 'alive') {
+				cell.isAlive = 1;
+			} else {
+				cell.isAlive = 0;
+			}
+		}
+	}
+
+}
+
+//draw initial grid
 drawGrid();
 
-console.log(grid[0]);
+function draw() {
+	runLife();
+	drawGrid();
+	//console.log('testing');
+
+	requestAnimationFrame(draw);
+}
+
+draw();
+
 
 
 
